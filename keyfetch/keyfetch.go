c965ce153c89587a9fc363ba6420f7e7ecd0318d
Rawat, Arvind <arvind.rawat@intel.com>
2019-02-27 14:59:56 +0530

    v1.0/features/ISecL-3648: Added go routine for fetching image decryption key and saving it in key cache.

--- keyfetch/keyfetch.go
+++ keyfetch/keyfetch.go
@@ -4,7 +4,18 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+	"time"
+
+	pool "github.com/Appboy/worker-pools"
 )
+
+type keyInfo struct {
+	keyURL    string
+	imageUUID string
+}
+
+// Global channel to communicate between secure-docker-plugin and CacheKey goroutine.
+var ch = make(chan keyInfo, 1)
 
 func getKeyHandle(keyURL string) string {
 
@@ -24,7 +35,49 @@ func cacheDecryptionKey(imageUUID, keyHandle string) error {
 	return nil
 }
 
-// FetchKey is used to fetch the decryption key from workload agent and store wrapped key in kernel keyring
+// CacheKey is used to call workload agent to fetch the kmskey and unwrap the key and store actual key  in kernel keyring
+func CacheKey() {
+	var (
+		keyHandle string
+		err       error
+	)
+	maxConcurrentWorkloads := 2
+	stalePoolExpiration := 5 * time.Minute
+	maxPoolLifetime := 10 * time.Minute
+
+	// workerpool manager is initialized
+	poolManager := pool.NewWorkerPoolManager(
+		maxConcurrentWorkloads, stalePoolExpiration, maxPoolLifetime,
+	)
+	for {
+		pool, doneUsing := poolManager.GetPool("pool 1", 1)
+		defer close(doneUsing)
+		pool.Submit(func() {
+			// Do anything here. Only maxConcurrentWorkloads will be allowed to execute concurrently per pool.
+			// This is useful for limiting concurrent usage of external resources.
+
+			// waiting for image id and key url from the plugin
+			keyInfo := <-ch
+			log.Println("keyInfo.keyURL: ", keyInfo.keyURL)
+			log.Println("keyInfo.imageUUID: ", keyInfo.imageUUID)
+
+			keyHandle = getKeyHandle(keyInfo.keyURL)
+			if keyHandle == "" {
+				log.Println("Unable to get keyHandle from key url", keyInfo.keyURL)
+			}
+
+			if keyHandle != "" {
+				err = cacheDecryptionKey(keyInfo.imageUUID, keyHandle)
+				if err != nil {
+					log.Println("Unable to store the key in the keyring", err)
+				}
+				log.Println("Key is present in keyring", keyHandle)
+			}
+		})
+	}
+}
+
+/*// FetchKey is used to fetch the decryption key from workload agent and store wrapped key in kernel keyring
 func FetchKey(keyURL, imageUUID string, keyfetchch chan bool) {
 
 	if keyURL == "" {
@@ -39,6 +92,27 @@ func FetchKey(keyURL, imageUUID string, keyfetchch chan bool) {
 		keyfetchch <- false
 		return
 	}
+
+	keyfetchch <- true
+}*/
+
+// FetchKey is used to fetch the decryption key from workload agent and store wrapped key in kernel keyring
+func FetchKey(keyURL, imageUUID string, keyfetchch chan bool) {
+
+	if keyURL == "" {
+		log.Println("Key URL is not specified in flavor.")
+		keyfetchch <- false
+		return
+	}
+
+	var ki keyInfo
+	ki.keyURL = keyURL
+	ki.imageUUID = imageUUID
+	log.Println("ki.keyURL: ", ki.keyURL)
+	log.Println("ki.imageUUID: ", ki.imageUUID)
+
+	// Triggering the goroutine by passing image id and key url on channel
+	ch <- ki
 
 	keyfetchch <- true
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os/user"
 	"strconv"
 
+	"secure-docker-plugin/keyfetch"
 	"secure-docker-plugin/plugin"
 
 	"github.com/docker/go-plugins-helpers/authorization"
@@ -16,9 +17,21 @@ const (
 	pluginSocket      = "/run/docker/plugins/secure-docker-plugin.sock"
 )
 
+func recovery() {
+
+	if r := recover(); r != nil {
+		log.Println("Recovered:", r)
+	}
+}
+
 func main() {
 	flDockerHost := flag.String("host", defaultDockerHost, "Specifies the host where docker is running")
 	flag.Parse()
+
+	defer recovery()
+
+	// CacheKey is goroutine, will run along side secure docker plugin to call workload agent to fetch the kmskey,unwrap the key and store the actual key in kernel keyring
+	go keyfetch.CacheKey()
 
 	// Create plugin instance
 	plugin, err := plugin.NewPlugin(*flDockerHost)
@@ -33,4 +46,7 @@ func main() {
 	if err := handler.ServeUnix(pluginSocket, gid); err != nil {
 		log.Fatal(err)
 	}
+
+	wait := make(chan struct{})
+	<-wait
 }
