1ec3d5db597a0c967a9ab465b840165068d265a1
R, Praveenraj <praveenraj.r@intel.com>
2019-08-29 13:09:24 -0700

    Added License headers

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+/*
+ * Copyright (C) 2019 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 import (
 	"os"
 	"strconv"
--- integrity/integrity.go
+++ integrity/integrity.go
@@ -1,5 +1,10 @@
 package integrity
 
+/*
+ * Copyright (C) 2019 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 import (
 	"log"
 	"os/exec"
